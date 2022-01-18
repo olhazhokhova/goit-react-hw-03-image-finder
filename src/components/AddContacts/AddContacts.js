e96a4a68167b1990e3de6bfa784ac72fcf144f1e
Olha Zhokhova <zhokhova.olha@gmail.com>
2022-01-18 21:33:10 +0200

    switch form to formik

--- src/components/AddContacts/AddContacts.js
+++ src/components/AddContacts/AddContacts.js
@@ -1,62 +1,59 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import PropTypes from 'prop-types';
 import s from './AddContacts.module.css';
 
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .trim()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan",
+    )
+    .required(),
+  number: Yup.string()
+    .trim()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
+    )
+    .required(),
+});
+
 class AddContacts extends React.Component {
-  state = {
-    name: '',
-    number: '',
-  };
-
-  hangleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.resetForm();
-  };
-
-  resetForm = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  handleSubmit = (values, { resetForm }) => {
+    this.props.onSubmit(values);
+    resetForm();
   };
 
   render() {
-    const { name, number } = this.state;
-
     return (
-      <form className={s.form} onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.hangleChange}
-          />
-        </label>
-        <label>
-          Number
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.hangleChange}
-          />
-        </label>
-        <button>Add contact</button>
-      </form>
+      <div className={s.formWrap}>
+        <Formik
+          initialValues={{ name: '', number: '' }}
+          validationSchema={validationSchema}
+          onSubmit={this.handleSubmit}
+        >
+          <Form autoComplete="off">
+            <label htmlFor="name">Name</label>
+            <Field type="text" name="name" />
+            <ErrorMessage name="name">
+              {msg => (
+                <div style={{ color: 'red', fontSize: '13px' }}>{msg}</div>
+              )}
+            </ErrorMessage>
+            <label htmlFor="name">Number</label>
+            <Field type="tel" name="number" />
+            <ErrorMessage name="number">
+              {msg => (
+                <div style={{ color: 'red', fontSize: '13px' }}>{msg}</div>
+              )}
+            </ErrorMessage>
+            <button type="submit">Add contact</button>
+          </Form>
+        </Formik>
+      </div>
     );
   }
 }
